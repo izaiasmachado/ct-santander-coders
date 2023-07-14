2e5ba852f2433ad28b291c93b31e98d6ccfb5071
Izaias Machado <izaiasmachado.dev@gmail.com>
2023-07-14 20:14:03 -0300

    feat: scanner

--- logica-programacao/aula02/src/App.java
+++ logica-programacao/aula02/src/App.java
@@ -1,11 +1,40 @@
+import java.util.Scanner;
+
 public class App {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
         ShoppingCart cart = new ShoppingCart();
 
-        cart.addItem("pera", 5);
-        cart.addItem("laranja", 10);
-
-        System.out.println("Total price: " + cart.getTotalPrice());
+        askForItems(cart);
+        printTotalPrice(cart);
     }
 
+    public static void askForItem(ShoppingCart cart) {
+        System.out.println("Digite o nome do: ");
+        String item = scanner.nextLine();
+
+        System.out.println("Digite a quantidade: ");
+        int quantity = scanner.nextInt();
+
+        cart.addItem(item, quantity);
+    }
+
+    public static void askForItems(ShoppingCart cart) {
+        boolean shouldAskForItem = true;
+
+        while (shouldAskForItem) {
+            askForItem(cart);
+
+            System.out.println("Você quer adicionar outro item? [s/n]");
+            String answer = scanner.nextLine();
+
+            shouldAskForItem = answer.equals("s");
+        }
+    }
+
+    public static void printTotalPrice(ShoppingCart cart) {
+        double totalPrice = cart.getTotalPrice();
+        System.out.printf("Preço total da compra: R$ %.2f\n", totalPrice);
+    }
 }

12d4086aa74fbd79a506ee7394a1cca675e8e801
Izaias Machado <izaiasmachado.dev@gmail.com>
2023-07-17 20:30:35 -0300

    fix: conditions third exercise

--- logica-programacao/aula03/src/exercicio03/Solution.java
+++ logica-programacao/aula03/src/exercicio03/Solution.java
@@ -5,11 +5,11 @@ public class Solution {
     private static double truncateFactor = Math.pow(10, truncatePlaces);
 
     public static double getRaisePercentage(double workedYears) {
-        if (workedYears >= 1 && workedYears <= 5) {
+        if (workedYears >= 1 && workedYears < 5) {
             return 0.01;
-        } else if (workedYears > 5 && workedYears <= 10) {
+        } else if (workedYears >= 5 && workedYears < 10) {
             return 0.015;
-        } else if (workedYears > 10) {
+        } else if (workedYears >= 10) {
             return 0.02;
         }
 
@@ -22,14 +22,10 @@ public class Solution {
         double salary = input[2];
 
         double raisePercentage = getRaisePercentage(workedYears);
-        System.out.println(raisePercentage);
 
         double inflationPercentage = inflation / 100;
-
-        System.out.println(inflationPercentage);
         double totalPercentage = raisePercentage + inflationPercentage;
 
-        System.out.println(totalPercentage);
         double newSalary = (1 + totalPercentage) * salary;
         double truncatedNewSalary = Math.round(newSalary * truncateFactor) / truncateFactor;
 

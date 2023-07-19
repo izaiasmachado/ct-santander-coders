901f9211566968509b550f1f77c900174318c1c2
Izaias Machado <izaiasmachado.dev@gmail.com>
2023-07-19 19:17:01 -0300

    feat: add test cases

--- logica-programacao/aula03/src/exercicio02/Solution.java
+++ logica-programacao/aula03/src/exercicio02/Solution.java
@@ -1,18 +1,47 @@
 package exercicio02;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Arrays;
 
 public class Solution {
-    public static double round(double input) {
-        double truncatePlaces = 2;
-        double truncateFactor = Math.pow(10, truncatePlaces);
+    public static void main(String[] args) {
+        double[][] entradas = {
+                { 100000.00, 12000.00, 20000.00 },
+                { 115500.00, 25000.00, 32500.00 },
+                { 121000.0, 14350.0, 21450.0 },
+                { 238500.0, 18350.0, 32450.0 },
+                { 53000.0, 2350.0, 8450.0 },
+                { 1250000.0, 124550.0, 225450.0 },
+                { 145000.0, 12550.0, 22450.0 },
+                { 173000.0, 12450.0, 22540.0 },
+                { 111000.0, 12455.0, 22550.0 },
+                { 123000.0, 8550.0, 25450.0 },
+                { 92000.0, 5450.0, 16450.0 }
+        };
 
-        BigDecimal bd = new BigDecimal(input * truncateFactor / truncateFactor);
-        bd = bd.setScale(3, RoundingMode.DOWN);
-        bd = bd.setScale(2, RoundingMode.HALF_DOWN);
+        double[][] saidas = {
+                { 12.00, 20.00 },
+                { 21.65, 28.14 },
+                { 11.86, 17.73 },
+                { 7.69, 13.61 },
+                { 4.43, 15.94 },
+                { 9.96, 18.04 },
+                { 8.66, 15.48 },
+                { 7.20, 13.03 },
+                { 11.22, 20.32 },
+                { 6.95, 20.69 },
+                { 5.92, 17.88 }
+        };
 
-        return bd.doubleValue();
+        for (int i = 0; i < entradas.length; i++) {
+            double[] resultado = custosCarro(entradas[i]);
+            double[] esperado = saidas[i];
+
+            System.out.println("Resultado: " + Arrays.toString(resultado));
+            System.out.println("Esperado: " + Arrays.toString(esperado));
+            System.out.println(Arrays.equals(resultado, esperado));
+            System.out.println();
+        }
     }
 
     public static double[] custosCarro(double[] input) {
@@ -30,16 +59,8 @@ public class Solution {
         return output;
     }
 
-    public static void main(String[] args) {
-        double[] input = {
-                238500,
-                18350,
-                32450
-
-        };
-        double[] output = custosCarro(input);
-
-        System.out.println(output[0]);
-        System.out.println(output[1]);
+    public static double round(double numero) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        return Double.parseDouble(decimalFormat.format(numero));
     }
 }
